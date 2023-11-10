5825942462e512b620ee575ae4bf948b233776aa
daniellenewby <danielleanne.newby@gmail.com>
2023-11-10 21:28:21 +0000

    updating CDMconnector to cran version

--- 2_Analysis/Tableone.R
+++ 2_Analysis/Tableone.R
@@ -128,22 +128,6 @@ tableone_all_cancers <- cdm$analysis %>%
         
       )
       
-  mutate(cohort_definition_id = 10) %>% 
-  summariseCharacteristics(
-    strata = list(c("sex"),c("age_gr"), c("sex", "age_gr" )),
-    minCellCount = 10,
-    ageGroup = list(c(18, 39), c(40, 49), c(50, 59), c(60, 69), c(70, 79), c(80, 150)),
-    tableIntersect = list(
-      "outcome" = list(
-        targetCohortTable = "outcome", value = "flag", window = c(0, 0) )
-    
-    
-    
-  )
-  ) %>% 
-  mutate(group_level = "All Cancers")
-
-
 
 info(logger, "CREATED TABLE ONE SUMMARY")
 
